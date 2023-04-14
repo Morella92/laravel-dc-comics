828efb98392d42c683d7651afe90c352abbd4240
Morella92 <morena.piem@outlook.com>
2023-04-14 15:44:49 +0200

    FEAT style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,7 +5,7 @@
     <h1>{{ $comic->title }}</h1>
     <p>{{ $comic->description }}</p>
     <img src="{{ $comic->thumb }}" alt="" width="300">
-    <p>Prezzo: {{ $comic->price }}</p>
+    <p class="price">Prezzo: {{ $comic->price }}</p>
     <p>Serie: {{ $comic->series }}</p>
     <p>Data di uscita: {{ $comic->sale_date }}</p>
     <p>Genere: {{ $comic->type }}</p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comics</title>
+
+    <style>
+      body {
+        font-family: 'Nunito', sans-serif;
+      }
+    </style>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+    
 </head>
 <body>
     @yield('content')
